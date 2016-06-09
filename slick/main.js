5df5738896729363cc6480f316b0628829d26203
negativenikolaj <ryan.young@cukeragency.com>
2016-06-09 11:24:00 -0700

    Create main.js

---
+++ slick/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.your-class').slick({
+    dots: true,
+    speed: 300,
+    slidesToShow: 1,
+    infinite: true,
+    arrows: true,
+    slidesToScroll: 3
+  });
+});
